f6f6d12ef7ad8b6688807b14dd4ce70118f86795
Sriman <sriman.mayandi@cogniwide.com>
2025-02-07 15:14:36 +0530

    Delete Backend directory

--- Backend/app.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from Backend import app
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
-
